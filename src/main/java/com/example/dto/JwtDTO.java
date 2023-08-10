92709ee4f7e8987f6948d998854c4fa03f5c761d
javohir <javohscript@gmail.com>
2023-08-10 22:30:33 +0500

    Added Security and build Crud

---
+++ src/main/java/com/example/dto/JwtDTO.java
@@ -0,0 +1,23 @@
+package com.example.dto;
+
+import com.example.enums.ProfileRole;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class JwtDTO {
+    private Integer id;
+    private ProfileRole role;
+    private String phone;
+
+    public JwtDTO(Integer id, ProfileRole role) {
+        this.id = id;
+        this.role = role;
+    }
+
+    public JwtDTO(String phone,ProfileRole role) {
+        this.role = role;
+        this.phone = phone;
+    }
+}
